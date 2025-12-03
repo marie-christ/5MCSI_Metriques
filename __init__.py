285a0a69a17cc9681a393d47e242b686e6654043
marie-christ <83068749+marie-christ@users.noreply.github.com>
2025-12-03 14:47:50 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,8 +8,6 @@ import sqlite3
 app = Flask(__name__)          
 @app.route("/contact/")
 def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-  
 
     return render_template("contact.html")
   
